a2f250f9a6409cc6132e3953015c4ab9fce29f9d
Emmanuel Adiba <eabaagah@gmail.com>
2022-05-20 02:00:31 +0000

    update

---
+++ src/main/java/com/aweperi/stockmanagement/StockManagement.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.aweperi.stockmanagement;
+
+/**
+ *
+ * @author Emmanuel Adiba
+ */
+public class StockManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
